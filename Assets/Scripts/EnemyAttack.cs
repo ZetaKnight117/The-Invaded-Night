a39bd78f548bd237119a1762496f61fd1d4e0434
ZetaKnight117 <primemaster@outlook.com>
2020-11-01 01:44:44 -0500

    enemy controller in progress

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -1,18 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsHover, whatIsPlayer;
+
+    //Patroling
+    public Vector3 hoverPoint;
+    bool hoverPointSet;
+    public float hoverPointRange;
+
+    //Attacking
+    public float timeBetwwenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    public void Awake()
     {
-        
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!player && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+
+    private void Patroling()
+    {
+        if (!hoverPointSet) SearchHoverPoint();
+
+        if (!hoverPointSet)
+            agent.SetDestination(hoverPoint);
+
+        Vector3 distanceToHoverPoint = transform.position - hoverPoint;
+
+        //Hoverpoint reached
+        if (distanceToHoverPoint.magnitude < 1f)
+            hoverPointSet = false;
+    }
+
+    private void SearchHoverPoint()
+    {
+        // Calculate random point range
+        float RandomZ = Random.Range(-hoverPointRange, hoverPointRange);
+        float RandomX = Random.Range(-hoverPointRange, hoverPointRange);
+
+        hoverPoint = new Vector3(transform.position.x + RandomX, transform.position.y, transform.position.z + RandomZ);
+    }
+
+    private void ChasePlayer()
+    {
+
+    }
+
+    private void AttackPlayer()
+    {
+
     }
 }
